81b4a559eecba9c93665af28ed886835ce3f0011
Joseph Oluchukwu <devjoseph016@gmail.com>
2026-05-20 17:17:40 +0100

    feat: add JoinResponse and PositionResponse interfaces for API responses

---
+++ src/entry/interfaces/join-response.interface.ts
@@ -0,0 +1,6 @@
+export interface JoinResponse {
+  id: string;
+  email: string;
+  position: number;
+  message: string;
+}
---
+++ src/entry/interfaces/position-response.interface.ts
@@ -0,0 +1,4 @@
+export interface PositionResponse {
+  email: string;
+  position: number;
+}
