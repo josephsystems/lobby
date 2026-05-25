45784bcb9df6dc61e33ac4815a6a157c4f8108dd
Joseph Oluchukwu <devjoseph016@gmail.com>
2026-05-25 13:48:34 +0100

    feat: add TrimStringsPipe to recursively trim string values in requests

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './shared/filters/global-exception.filter';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
+import { TrimStringsPipe } from './shared/pipes/trim-strings.pipe';
 
 async function bootstrap() {
   const emailEnabled = process.env['EMAIL_ENABLED'] === 'true';
   const app = await NestFactory.create(AppModule.register(emailEnabled));
 
   app.useGlobalPipes(
+    new TrimStringsPipe(),
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
